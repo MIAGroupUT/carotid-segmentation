097ca83d2848b898a35f18f397189e234c4852eb
Elina Thibeau-Sutre <elina.ts@free.fr>
2023-02-20 14:57:36 +0100

    Add script to fetch models
    Former-commit-id: 9001e00a8faa9b094ab0989dee3f50fefa698932

---
+++ models/fetch.sh
@@ -0,0 +1,11 @@
+cd models/
+
+# Retrieve contour models
+curl https://surfdrive.surf.nl/files/index.php/s/evopHTobixCuf3t/download -o contour_transform.tar
+tar -xvf contour_transform.tar
+rm contour_transform.tar
+
+# Retrieve heatmap models
+curl https://surfdrive.surf.nl/files/index.php/s/1wG3WbCuEy34NsU/download -o heatmap_transform.tar
+tar -xvf heatmap_transform.tar
+rm heatmap_transform.tar
