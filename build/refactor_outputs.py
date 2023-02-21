f6af079ca47e3a4bc0ab8f9a73e7bb05b592280c
Elina Thibeau-Sutre <elina.ts@free.fr>
2023-02-21 14:18:50 +0100

    Print parameters
    Former-commit-id: bbd9f6bfb39c14abb5d74ea80cc06d4fbd68fd36

--- build/refactor_outputs.py
+++ build/refactor_outputs.py
@@ -1,8 +1,7 @@
-from carotid.utils import build_dataset
+from carotid.utils import build_dataset, read_json, write_json
 from monai.data import ITKWriter
 import numpy as np
 from os import makedirs, path
-import json
 import argparse
 
 parser = argparse.ArgumentParser(description='Optional app description')
@@ -16,22 +15,12 @@ output_dir = args.output_dir
 tmp_dir = path.join(output_dir, "tmp")
 makedirs(path.join(output_dir, "images"), exist_ok=True)
 
-results_dict = {
-    "outputs": [],
-    "inputs": [],
-    "error_messages": [],
-}
-
 writer = ITKWriter(output_dtype="uint8")
 dataset = build_dataset(raw_dir=input_dir, segmentation_dir=tmp_dir)
+pipeline_dict = read_json(path.join(tmp_dir, "pipeline_parameters.json"))
+write_json(pipeline_dict, path.join(output_dir, "results.json"))
 for sample in dataset:
     participant_id = sample["participant_id"]
-    results_dict["inputs"].append(
-        dict(type="metaio_image", filename=f"{participant_id}.mha")
-    )
-    results_dict["outputs"].append(
-        dict(type="metaio_image", filename=f"{participant_id}.mha")
-    )
     wall_segmentation = 0
     for side in ["left", "right"]:
         wall_segmentation += sample[f"{side}_segmentation"][:1]
@@ -40,5 +29,3 @@ for sample in dataset:
     writer.set_metadata({"affine": sample["image"].affine})
     writer.write(path.join(output_dir, "images", f"{participant_id}.mha"))
 
-with open(path.join(output_dir, "results.json"), "w") as f:
-    json.dump(results_dict, f)
