e26c06280c8bee7a556cb07338d5d1a40458111e
Elina Thibeau-Sutre <elina.ts@free.fr>
2023-02-21 16:50:20 +0100

    Fix center inversion in contour transform
    Former-commit-id: 245601b8c0e53716e12c0b3b22821437bd36cc53

--- build/refactor_outputs.py
+++ build/refactor_outputs.py
@@ -23,7 +23,7 @@ for sample in dataset:
     participant_id = sample["participant_id"]
     wall_segmentation = 0
     for side in ["left", "right"]:
-        wall_segmentation += sample[f"{side}_segmentation"][:1]
+        wall_segmentation += sample[f"{side}_segmentation"][1:]
     wall_segmentation = np.clip(wall_segmentation, 0, 1)
     writer.set_data_array(wall_segmentation)
     writer.set_metadata({"affine": sample["image"].affine})
