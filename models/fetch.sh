0d8bd2be5c16ecf3dab450e56d144eac3b3c91de
Elina Thibeau-Sutre <elina.ts@free.fr>
2023-02-22 16:14:12 +0100

    Use unzip instead of tar
    Former-commit-id: 0f1e9d0d3f54bc86525b296554b9aeaf92dd83ce

--- models/fetch.sh
+++ models/fetch.sh
@@ -3,7 +3,7 @@ cd models/
 if [ ! -d "contour_transform" ]; then
     # Retrieve contour models
     curl https://surfdrive.surf.nl/files/index.php/s/evopHTobixCuf3t/download -o contour_transform.tar
-    tar -xvf contour_transform.tar
+    unzip contour_transform.tar
     rm contour_transform.tar
 else
     echo "models for contour_transform already found."
@@ -12,7 +12,7 @@ fi
 if [ ! -d "heatmap_transform" ]; then
     # Retrieve heatmap models
     curl https://surfdrive.surf.nl/files/index.php/s/1wG3WbCuEy34NsU/download -o heatmap_transform.tar
-    tar -xvf heatmap_transform.tar
+    unzip heatmap_transform.tar
     rm heatmap_transform.tar
 else
     echo "models for heatmap_transform already found."
@@ -21,7 +21,7 @@ fi
 if [ ! -d "contour_transform_dropout" ]; then
     # Retrieve heatmap models
     curl https://surfdrive.surf.nl/files/index.php/s/P9uMnfKWSbenfDx/download -o contour_transform_dropout.tar
-    tar -xvf contour_transform_dropout.tar
+    unzip contour_transform_dropout.tar
     rm contour_transform_dropout.tar
 else
     echo "models for contour_transform_dropout already found."
