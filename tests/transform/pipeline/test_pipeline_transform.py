397198349cde76566d52ac92243c04e77730cfcf
Elina Thibeau-Sutre <elina.ts@free.fr>
2023-06-09 14:48:33 +0200

    print difference

--- tests/transform/pipeline/test_pipeline_transform.py
+++ tests/transform/pipeline/test_pipeline_transform.py
@@ -35,7 +35,8 @@ def test_pipeline():
     check_equal_parameters(ref_params, out_params)
 
     dice_df = pd.read_csv(path.join(tmp_dir, "compare_contour_dice.tsv"), sep="\t")
-    print(dice_df)
+    print(dice_df[dice_df.object == "lumen"].dice_score.min())
+    print(dice_df[dice_df.object == "wall"].dice_score.min())
     assert (dice_df.dice_score > 0.99).all()
 
     shutil.rmtree(tmp_dir)
